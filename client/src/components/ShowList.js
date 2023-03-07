cedbece238a77ed5dabed277ae3651a466399088
Rohit Ranjan <rohitr1612@gamil.com>
2023-03-07 20:00:52 +0100

    refractor show more

--- client/src/components/ShowList.js
+++ client/src/components/ShowList.js
@@ -1,38 +1,51 @@
-import React, {useState} from 'react';
-import ShowListItem from './ShowListItem';
-import {textLabel} from "../labels/TextLables"; 
+import React, { useState } from "react";
+import ShowListItem from "./ShowListItem";
+import { textLabel } from "../labels/TextLables";
 
 function ShowList(props = {}) {
+  const initialOffset = 8;
+  const increments = 8;
+  const [offset, setOffset] = useState(initialOffset);
 
-    const initialOffset = 8;
-    const [offset, setOffset] = useState(initialOffset);
+  const { offersList = [] } = props;
+  const offersToDisplay = offersList.slice(0, offset);
 
-    return (
-        <div aria-labelledby='list-description' className='showlist_container' >
-            <div className='showlist_descripton'>
-                <span className='showlist_header' id="list-description"> {textLabel.OFFERS_HEADER}</span>
-                <p className='showlist_detail'> {textLabel.SELECT_FILTER_LABEL} </p>
-            </div>
-            <div className="showlist__wrapper">
-                {(props.offersList.length >0) ? props.offersList.map((item, index) => {
-                    if (index < offset) {
-                        return (
-                            <ShowListItem data={item} key={item.uuid} airportCodeToCityMap={props.airportCodeToCityMap}/>
-                        );
-                    }
-                    return null;
-                    
-                }): 
-                <div className='showlist_empty' aria-labelledby='emptyOffer'>
-                    <span id="emptyOffer">{textLabel.NO_OFFERS}</span>
-                </div>
-                }
-            </div>
-            {(props.offersList.length >= offset) ? 
-                ( <button className="btn-blue" onClick={() => setOffset((offset) => offset + 10)}>{textLabel.SHOW_MORE}</button>) : 
-                null}       
-        </div>
-    );
+  return (
+    <div aria-labelledby="list-description" className="showlist_container">
+      <div className="showlist_descripton">
+        <span className="showlist_header" id="list-description">
+          {" "}
+          {textLabel.OFFERS_HEADER}
+        </span>
+        <p className="showlist_detail"> {textLabel.SELECT_FILTER_LABEL} </p>
+      </div>
+      <div className="showlist__wrapper">
+        {offersToDisplay.length > 0 ? (
+          offersToDisplay.map((item) => {
+            return (
+              <ShowListItem
+                data={item}
+                key={item.uuid}
+                airportCodeToCityMap={props.airportCodeToCityMap}
+              />
+            );
+          })
+        ) : (
+          <div className="showlist_empty" aria-labelledby="emptyOffer">
+            <span id="emptyOffer">{textLabel.NO_OFFERS}</span>
+          </div>
+        )}
+      </div>
+      {offersList.length > offset ? (
+        <button
+          className="btn-blue"
+          onClick={() => setOffset((offset) => offset + increments)}
+        >
+          {textLabel.SHOW_MORE}
+        </button>
+      ) : null}
+    </div>
+  );
 }
-  
+
 export default ShowList;
